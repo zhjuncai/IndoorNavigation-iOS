7edbed69683bfa89e046e4b120125b934a0bcb4d
Zion <751735080@qq.com>
2015-03-17 15:44:30 +0800

    Add compass

--- IndoorNavigation/PathBuilderView.m
+++ IndoorNavigation/PathBuilderView.m
@@ -8,17 +8,20 @@
 
 #import "PathBuilderView.h"
 #import "ShapeView.h"
+#import <CoreLocation/CoreLocation.h>
 
 static CGFloat const kDistanceThreshold = 10.0;
 static CGFloat const kPointDiameter = 7.0;
 
-@interface PathBuilderView ()
+@interface PathBuilderView () <CLLocationManagerDelegate>
 @property (nonatomic, strong) NSMutableArray *points;
 @property (nonatomic, strong) NSValue *prospectivePointValue;
 @property (nonatomic) NSUInteger indexOfSelectedPoint;
 @property (nonatomic) CGVector touchOffsetForSelectedPoint;
 @property (nonatomic, strong) NSTimer *pressTimer;
 @property (nonatomic) BOOL ignoreTouchEvents;
+
+@property (nonatomic , strong) CALayer *naviIcon;
 
 @end
 
@@ -53,6 +56,11 @@ static CGFloat const kPointDiameter = 7.0;
         _pointsShapeView.opaque = NO;
         _pointsShapeView.translatesAutoresizingMaskIntoConstraints = NO;
         [self addSubview:_pointsShapeView];
+        
+        self.naviIcon = [[CALayer alloc] init];
+        self.naviIcon.frame = CGRectMake(384, 892,30,30);
+        self.naviIcon.contents = (id)[[UIImage imageNamed:@"location.png"] CGImage];
+        [self.layer addSublayer:self.naviIcon];
         
     }
     return self;}
@@ -112,6 +120,25 @@ static CGFloat const kPointDiameter = 7.0;
     self.prospectivePathShapeView.shapeLayer.path = nil;
     self.pathShapeView.shapeLayer.path = nil;
     [self.points removeAllObjects];
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
+    
+    CGFloat headings = M_PI*newHeading.trueHeading/180.0f;
+    //CGFloat headings = newHeading.trueHeading;
+    CABasicAnimation* anim = [CABasicAnimation animationWithKeyPath:@"transform"];
+    
+    CATransform3D fromValue = self.naviIcon.transform;
+    anim.fromValue = [NSValue valueWithCATransform3D:fromValue];
+    
+    CATransform3D toValue = CATransform3DMakeRotation(headings, 0, 0, 1);
+    anim.toValue = [NSValue valueWithCATransform3D:toValue];
+    anim.duration = 0.2;
+    anim.removedOnCompletion = YES;
+    self.naviIcon.transform = toValue;
+    [self.naviIcon addAnimation:anim forKey:nil];
+    
+
 }
 
 @end
